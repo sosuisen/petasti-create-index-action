bff1432d3d12f852ddaa459d54e6ae73ac8ed905
sosuisen <hidekazu.kubota@gmail.com>
2021-11-22 15:31:50 +0900

    fix: check path

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const indexFileName = core.getInput('index-file-name');
+  console.log(`index: ${indexFileName}!`);
+
+  console.log(`path: ${path.resolve('./')}`);
+
+  // Get note list
+  /*
+  const noteDirList = 
+  for (const noteDir of noteDirList) {
+    count++;
+    // eslint-disable-next-line no-await-in-loop
+    const prop: NoteProp = (await noteDir.get('prop')) as NoteProp;
+    const pathArr = noteDir.collectionPath.split('/'); // collectionPath is note/nXXXXXX/
+    prop._id = pathArr[1]; // Set note id instead of 'prop'.
+    initialNoteState.set(prop._id, prop);
+
+    if (startingProgressBar) {
+      startingProgressBar.detail =
+        MESSAGE('loadingNoteProgressBarBody') + `(${count}/${noteDirList.length})`;
+    }
+  }
+  */
 } catch (error) {
   core.setFailed(error.message);
 }
