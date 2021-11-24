1e7b37f903d557f2512a75687fc765cfd09f8aea
sosuisen <hidekazu.kubota@gmail.com>
2021-11-24 17:02:16 +0900

    fix: lint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const yaml = require('js-yaml');
 /**
  * Parse YAML Front Matter Markdown
  */
-function parseFrontMatterMarkdown (text) {
+function parseFrontMatterMarkdown(text) {
   const mdArray = text.split('\n');
   let firstLine = '_';
   let yamlText = '';
@@ -36,8 +36,8 @@ function parseFrontMatterMarkdown (text) {
       markdownText += mdArray[i];
       if (firstLine === '_' && !(mdArray[i] === '' || mdArray[i].match(/^\s+\n?$/) || mdArray[i].match(/^```\n?$/))) {
         firstLine = mdArray[i];
-        firstLine = firstLine.replace(/^#+?\s/,'');
-        firstLine = firstLine.replace(/^\*+?\s/,'');
+        firstLine = firstLine.replace(/^#+?\s/, '');
+        firstLine = firstLine.replace(/^\*+?\s/, '');
         break;
       }
     }
@@ -48,14 +48,14 @@ function parseFrontMatterMarkdown (text) {
     jsonDoc._body = firstLine;
     return jsonDoc;
   } catch {
-    return undefined;  
+    return undefined;
   }
 }
 
 /**
  * Parse YAML Front Matter
  */
-function parseFrontMatter (text) {
+function parseFrontMatter(text) {
   const mdArray = text.split('\n');
   let yamlText = '';
   let startFrontMatter = false;
@@ -78,7 +78,7 @@ function parseFrontMatter (text) {
       yamlText += mdArray[i];
     }
     else if (endFrontMatter) {
-        break;
+      break;
     }
   }
 
@@ -86,16 +86,16 @@ function parseFrontMatter (text) {
     const jsonDoc = yaml.load(yamlText);
     return jsonDoc;
   } catch {
-    return undefined;  
+    return undefined;
   }
 }
 
 /**
  * Get local date and time
  */
-function getLocalDateAndTime (dateStr, offset) {
+function getLocalDateAndTime(dateStr, offset) {
   date = new Date(dateStr.replace(' ', 'T') + '.000z');
-    return new Date(date.getTime() + offset * 60 * 1000)
+  return new Date(date.getTime() + offset * 60 * 1000)
     .toISOString()
     .replace(/^(.+?)T(.+?)\..+?$/, '$1 $2');
 }
@@ -135,7 +135,7 @@ try {
       const noteProperty = yaml.load(notePropertyYAML);
       notePropsSorted.push(noteProperty);
     }
-    catch(err) {
+    catch (err) {
       console.log(err);
     }
   }
@@ -147,7 +147,7 @@ try {
 
   let noteIndexText = `# [${titleName}](../${indexFileName})\n\n`;
   noteIndexText += `## [Note](./${indexFileName})\n\n`;
-  for (let i=0; i<notePropsSorted.length; i++) {
+  for (let i = 0; i < notePropsSorted.length; i++) {
     noteIndexText += `- [${notePropsSorted[i].name}](./${notePropsSorted[i]._id.replace('note/', '').replace('/prop', `/${indexFileName}`)})\n`;
   }
   writeFileSync('note/' + indexFileName, noteIndexText);
@@ -156,7 +156,7 @@ try {
    * Generate card index
    * Cards are sorted by modified-date in descending order.
    */
-  for (let i=0; i<notePropsSorted.length; i++) {
+  for (let i = 0; i < notePropsSorted.length; i++) {
     const noteProp = notePropsSorted[i];
     const noteDir = `./${noteProp._id.replace('/prop', '/')}`;
     const cardSketchDirs = readdirSync(noteDir);
@@ -171,7 +171,7 @@ try {
         const cardBodyProperty = parseFrontMatterMarkdown(cardBodyPropertyYFMMD);
         cardBodyPropsSorted.push(cardBodyProperty);
       }
-      catch(err) {
+      catch (err) {
         console.log(err);
       }
     }
@@ -180,11 +180,11 @@ try {
       else if (a.date.modifiedDate < b.date.modifiedDate) return 1;
       return 0;
     });
-  
+
     let cardIndexText = `# [${titleName}](../../${indexFileName})\n\n`;
     cardIndexText += `## [Note](../${indexFileName})\n\n`;
     cardIndexText += `### [${noteProp.name}](./${indexFileName})\n\n`;
-    for (let i=0; i<cardBodyPropsSorted.length; i++) {
+    for (let i = 0; i < cardBodyPropsSorted.length; i++) {
       cardIndexText += `- [${cardBodyPropsSorted[i]._body}](../../${cardBodyPropsSorted[i]._id}.md) (${getLocalDateAndTime(cardBodyPropsSorted[i].date.modifiedDate, timezoneOffsetMinutes)})\n`;
     }
     writeFileSync(noteDir + indexFileName, cardIndexText);
@@ -193,30 +193,30 @@ try {
   /**
    * Generate snapshot index
    */
-   const snapshotFiles = readdirSync('./snapshot/');
-   const snapshotPropsSorted = [];
-   for (const snapshotFile of snapshotFiles) {
-     try {
-       const snapshotPropertyYFMMD = readFileSync('./snapshot/' + snapshotFile, 'utf8');
-       const snapshotProperty = parseFrontMatter(snapshotPropertyYFMMD);
-       if (snapshotProperty !== undefined) snapshotPropsSorted.push(snapshotProperty);
-     }
-     catch(err) {
-       console.log(err);
-     }
-   }
-   snapshotPropsSorted.sort((a, b) => {
-     if (a.createdDate > b.createdDate) return -1;
-     else if (a.createdDate < b.createdDate) return 1;
-     return 0;
-   });
- 
-   let snapshotIndexText = `# [${titleName}](../${indexFileName})\n\n`;
-   snapshotIndexText += `## [Snapshot](./${indexFileName})\n\n`;
-   for (let i=0; i<snapshotPropsSorted.length; i++) {
-     snapshotIndexText += `- [${snapshotPropsSorted[i].name}](../${snapshotPropsSorted[i]._id}.md) (${getLocalDateAndTime(snapshotPropsSorted[i].createdDate, timezoneOffsetMinutes)})\n`;
-   }
-   writeFileSync('snapshot/' + indexFileName, snapshotIndexText);
+  const snapshotFiles = readdirSync('./snapshot/');
+  const snapshotPropsSorted = [];
+  for (const snapshotFile of snapshotFiles) {
+    try {
+      const snapshotPropertyYFMMD = readFileSync('./snapshot/' + snapshotFile, 'utf8');
+      const snapshotProperty = parseFrontMatter(snapshotPropertyYFMMD);
+      if (snapshotProperty !== undefined) snapshotPropsSorted.push(snapshotProperty);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+  snapshotPropsSorted.sort((a, b) => {
+    if (a.createdDate > b.createdDate) return -1;
+    else if (a.createdDate < b.createdDate) return 1;
+    return 0;
+  });
+
+  let snapshotIndexText = `# [${titleName}](../${indexFileName})\n\n`;
+  snapshotIndexText += `## [Snapshot](./${indexFileName})\n\n`;
+  for (let i = 0; i < snapshotPropsSorted.length; i++) {
+    snapshotIndexText += `- [${snapshotPropsSorted[i].name}](../${snapshotPropsSorted[i]._id}.md) (${getLocalDateAndTime(snapshotPropsSorted[i].createdDate, timezoneOffsetMinutes)})\n`;
+  }
+  writeFileSync('snapshot/' + indexFileName, snapshotIndexText);
 } catch (error) {
   core.setFailed(error.message);
 }
